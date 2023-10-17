6ba24775d6244aa3932e686321db5dbc6080dfaa
Edilbert Crist Jaguimit <106310773+ZxBaymax@users.noreply.github.com>
2023-10-18 00:31:53 +0800

    student registration

--- Enrollment_System_DBMS/Dashboard.Designer.cs
+++ Enrollment_System_DBMS/Dashboard.Designer.cs
@@ -30,14 +30,16 @@ namespace Enrollment_System_DBMS
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel3 = new System.Windows.Forms.Panel();
+            this.addStudent1 = new Enrollment_System_DBMS.Student_Controls.AddStudent();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -49,6 +51,16 @@ namespace Enrollment_System_DBMS
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(960, 52);
             this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(376, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enrollment System";
             // 
             // panel2
             // 
@@ -62,16 +74,16 @@ namespace Enrollment_System_DBMS
             this.panel2.Size = new System.Drawing.Size(200, 476);
             this.panel2.TabIndex = 1;
             // 
-            // button1
+            // button3
             // 
-            this.button1.BackColor = System.Drawing.Color.Gold;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(0, 0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(200, 48);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Dashboard";
-            this.button1.UseVisualStyleBackColor = false;
+            this.button3.BackColor = System.Drawing.Color.Gold;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(0, 108);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(200, 48);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Student Records";
+            this.button3.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
@@ -84,34 +96,34 @@ namespace Enrollment_System_DBMS
             this.button2.Text = "Add Student";
             this.button2.UseVisualStyleBackColor = false;
             // 
-            // button3
+            // button1
             // 
-            this.button3.BackColor = System.Drawing.Color.Gold;
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(0, 108);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(200, 48);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Student Records";
-            this.button3.UseVisualStyleBackColor = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(320, 14);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(208, 25);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Enrollment System";
+            this.button1.BackColor = System.Drawing.Color.Gold;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Dashboard";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // panel3
             // 
+            this.panel3.Controls.Add(this.addStudent1);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel3.Location = new System.Drawing.Point(200, 52);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(760, 476);
             this.panel3.TabIndex = 2;
+            // 
+            // addStudent1
+            // 
+            this.addStudent1.BackColor = System.Drawing.Color.White;
+            this.addStudent1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.addStudent1.Location = new System.Drawing.Point(0, 0);
+            this.addStudent1.Name = "addStudent1";
+            this.addStudent1.Size = new System.Drawing.Size(760, 476);
+            this.addStudent1.TabIndex = 0;
             // 
             // Dashboard
             // 
@@ -127,6 +139,7 @@ namespace Enrollment_System_DBMS
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -136,9 +149,10 @@ namespace Enrollment_System_DBMS
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+        private Student_Controls.AddStudent addStudent1;
     }
 }
--- Enrollment_System_DBMS/Student Controls/AddStudent.cs
+++ Enrollment_System_DBMS/Student Controls/AddStudent.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,143 @@ namespace Enrollment_System_DBMS.Student_Controls
 {
     public partial class AddStudent : UserControl
     {
+        int ID { get; set; }
+        string StudentID { get; set; }
+        string Photo { get; set; }
+        string Firstname { get; set; }
+        string Middlename { get; set; }
+        string Lastname { get; set; }
+        string Age { get; set; }
+        string BirthDate { get; set; }
+        string PlaceBirth { get; set; }
+        string Address { get; set; }
+        string Email { get; set; }
+        string Mobile { get; set; }
+        string Gender { get; set; }
+        string Program { get; set; }
+        int GenderID { get; set; }
+        int ProgramID { get; set; } 
+        int AcademicID { get; set; }
+        int SubjectID { get; set; }
+        bool emptyGender = false;
+
+        EnrollmentDBDataContext db = new EnrollmentDBDataContext();
+
         public AddStudent()
         {
             InitializeComponent();
+        }
+
+        private void BtnUploadPhoto_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "jpg files(*.jpg)|*.jpg| PNG files(*.png)|*.png| All files(*.*)|*.*";
+                if(dialog.ShowDialog() == DialogResult.OK)
+                {
+                    Photo = dialog.FileName;
+                    LblPhoto.ImageLocation = Photo;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("An error occured", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void BtnRegister_Click(object sender, EventArgs e)
+        {
+            if (!isTextEmpty())
+            {
+                DateTime birthDate = DpBirthDate.Value;
+                TimeSpan age = DateTime.Now - birthDate;
+                int currentAge = (int)(age.TotalDays / 365.25);
+                string email = TxtEmail.Text;
+                string mobile = TxtMobile.Text;
+                bool isEmail = Regex.IsMatch(email, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase);
+                bool isNumeber = Regex.IsMatch(mobile, @"^(09|\+639)\d{9}$");
+                if (isNumeber)
+                {
+                    if (isEmail)
+                    {
+                        try
+                        {
+                            byte[] photoBytes = null;
+                            if (!string.IsNullOrEmpty(Photo))
+                            {
+                                try
+                                {
+                                    photoBytes = File.ReadAllBytes(Photo);
+                                }
+                                catch (Exception ex)
+                                {
+                                    MessageBox.Show($"Error reading the photo file: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                    return; // Exit the method to prevent further execution
+                                }
+                            }
+                            db.SP_INSERT(photoBytes, TxtFirstname.Text, TxtMiddlename.Text, TxtLastname.Text, currentAge.ToString(), birthDate, TxtPlaceBirth.Text, TxtAddress.Text, email, mobile, Gender, DateTime.Now, DateTime.Now);
+                            emptyGender = true;
+                            ClearText();
+                            MessageBox.Show("Successfully Added!", "Save", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            //btlViewData.DataSource = db.sp_view();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show($"An Error Occurred: {ex}", "Database Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid Email!", "Invalid", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Mobile Number!", "Invalid", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Input Field Are Empty", "Empty");
+            }
+        }
+        private void ClearText()
+        {
+            TxtFirstname.Text = "";
+            TxtMiddlename.Text = "";
+            TxtLastname.Text = "";
+            DpBirthDate.Value = DateTime.Now;
+            TxtAddress.Text = "";
+            TxtMobile.Text = "";
+            TxtEmail.Text = "";
+            TxtPlaceBirth.Text = "";
+            CbGender.Text = "";
+        }
+        private bool isTextEmpty()
+        {
+            if (TxtFirstname.Text == "" ||
+            TxtLastname.Text == "" ||
+            TxtAddress.Text == "" ||
+            TxtMobile.Text == "" ||
+            TxtEmail.Text == "" ||
+            DpBirthDate.Value == DateTime.Now ||
+            TxtPlaceBirth.Text == "" ||
+            CbGender.Text == "")
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void CbGender_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!emptyGender)
+            {
+                Gender = CbGender.Text;
+            }
+            else
+                CbGender.Text = "";
         }
     }
 }
